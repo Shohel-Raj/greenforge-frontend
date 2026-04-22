d00ef9da1f86a1ce9f469514431058bba986ec56
Shohel-Raj <shohel87.dev@gmail.com>
2026-04-22 16:08:45 +0600

    create axios instance, refresh token functionality and setup  routes config

---
+++ src/lib/authUtils.ts
@@ -0,0 +1,90 @@
+export type UserRole =  "ADMIN" | "MEMBER" ;
+
+export const authRoutes = [ "/login", "/register", "/forgot-password", "/reset-password", "/verify-email" ];
+
+export const isAuthRoute = (pathname : string) => {
+    return authRoutes.some((router : string) => router === pathname);
+}
+
+export type RouteConfig = {
+    exact : string[],
+    pattern : RegExp[]
+}
+
+export const commonProtectedRoutes : RouteConfig = {
+    exact : ["/my-profile", "/change-password"],
+    pattern : []
+}
+
+export const memberProtectedRoutes : RouteConfig = {
+    pattern: [/^\/member\/dashboard/ ], // Matches any path that starts with /doctor/dashboard
+    exact : []
+}
+
+export const adminProtectedRoutes : RouteConfig = {
+    pattern: [/^\/admin\/dashboard/ ], // Matches any path that starts with /admin/dashboard
+    exact : []
+}
+
+
+
+
+
+export const isRouteMatches = (pathname : string, routes : RouteConfig) => {
+    if(routes.exact.includes(pathname)) {
+        return true;
+    }
+    return routes.pattern.some((pattern : RegExp) => pattern.test(pathname));
+}
+
+export const getRouteOwner = (pathname : string) : "ADMIN" | "DOCTOR" | "MEMBER" | "COMMON" | null => {
+    if(isRouteMatches(pathname, memberProtectedRoutes)) {
+        return "MEMBER";
+    }
+
+    // if (isRouteMatches(pathname, superAdminProtectedRoutes)) {
+    //     return "SUPER_ADMIN";
+    // }
+
+    if(isRouteMatches(pathname, adminProtectedRoutes)) {
+        return "ADMIN";
+    }
+    
+
+
+    if(isRouteMatches(pathname, commonProtectedRoutes)) {
+        return "COMMON";
+    }
+
+    return null; // public route
+}
+
+export const getDefaultDashboardRoute = (role : UserRole) => {
+    if(role === "ADMIN" ) {
+        return "/admin/dashboard";
+    }
+    if(role === "MEMBER") {
+        return "/member/dashboard";
+    }
+
+    return "/";
+}
+
+export const isValidRedirectForRole = (redirectPath : string, role : UserRole) => {
+    const unifySuperAdminAndAdminRole = role === "ADMIN" ? "ADMIN" : role;
+
+    role = unifySuperAdminAndAdminRole;
+
+    const sanitizedRedirectPath = redirectPath.split("?")[0] || redirectPath;
+    const routeOwner = getRouteOwner(sanitizedRedirectPath);
+
+    if(routeOwner === null || routeOwner === "COMMON"){
+        return true;
+    }
+
+    if(routeOwner === role){
+        return true;
+    }
+
+    return false;
+}
