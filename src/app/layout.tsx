c7a26ee11a756c9778e6dae5f17d066003b05324
Shohel-Raj <shohel87.dev@gmail.com>
2026-04-22 16:43:14 +0600

    QueryProvider Added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import QueryProviders from "@/providers/QueryProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,8 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Green Forge",
-  description: "GreenForge is a backend API for a sustainability-focused platform where users can share, manage, and explore eco-friendly ideas, with features like authentication, voting, comments, payments, and admin moderation.",
+  description:
+    "GreenForge is a backend API for a sustainability-focused platform where users can share, manage, and explore eco-friendly ideas, with features like authentication, voting, comments, payments, and admin moderation.",
 };
 
 export default function RootLayout({
@@ -27,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-          {children}
+        <QueryProviders>{children}</QueryProviders>
       </body>
     </html>
   );
