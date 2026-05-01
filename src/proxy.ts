a5ed41a81bb71e2398d2725f83cd2a4f7ae26b99
Shohel-Raj <shohel87.dev@gmail.com>
2026-05-01 19:39:41 +0600

    proxy added for secure dashboard access

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,11 +1,77 @@
-import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  return NextResponse.redirect(new URL('/home', request.url))
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+import { getUserInfo } from "./services/auth.services";
+export default async function middleware(request: NextRequest) {
+  const pathname = request.nextUrl.pathname;
+
+
+  // Skip verify-email
+  if (pathname.startsWith("/verify-email")) {
+    return NextResponse.next();
+  }
+
+  // ✅ Use correct cookie name
+  // const sessionToken = request.cookies.get("__Secure-better-auth.session_data");
+  const sessionToken = request.cookies.get("better-auth.session_token");
+  
+  const userInfo = await getUserInfo()
+
+
+  const role = userInfo?.role;
+
+
+  // Not logged in
+  if (!sessionToken) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // ===============================
+  // ADMIN ROUTE
+  // ===============================
+  if (pathname.startsWith("/admin")) {
+    if (role === "MEMBER") {
+      return NextResponse.redirect(
+        new URL("/member/dashboard", request.url)
+      );
+    }
+
+
+  }
+
+  // ===============================
+  // Member ROUTE
+  // ===============================
+  if (pathname.startsWith("/member")) {
+    if (role === "ADMIN") {
+      return NextResponse.redirect(
+        new URL("/admin/dashboard", request.url)
+      );
+    }
+  }
+
+  // ===============================
+  // USER ROUTE
+  // ===============================
+  if (pathname.startsWith("/dashboard")) {
+    if (role === "ADMIN") {
+      return NextResponse.redirect(
+        new URL("/admin/dashboard", request.url)
+      );
+    }
+    if (role === "MEMBER") {
+      return NextResponse.redirect(
+        new URL("/member/dashboard", request.url)
+      );
+    }
+  }
+
+  return NextResponse.next();
 }
- 
+
 export const config = {
-  matcher: '/about/:path*',
-}
+  matcher: [
+    "/admin/dashboard/:path*",
+    "/member/dashboard/:path*",
+    "/dashboard/:path*",
+  ],
+};
